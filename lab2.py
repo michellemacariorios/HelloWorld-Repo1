532ab616a41de5f5ad54d80f92ba373a1d524c44
Michelle Macario-Rios <113188339+michellemacariorios@users.noreply.github.com>
2022-10-25 11:34:05 -0500

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,20 @@
+# 1
+temp = eval(input("Enter temperature in Celsius: "))
+temp = 9/5*temp+32
+print("The temperature is  "+str(temp)+" Fahrenheit.")
+
+#2
+num1 = eval(input('Enter first number: '))
+num2 = eval(input('Enter second number: '))
+average = (num1+num2)/2
+average_of_two_numbers = input("The average is "+str(average)+".")
+
+#3
+print("3+4")
+print("The value of 3+4 is", 3 + 4)
+print("The value of 3 +4 is", 3 + 4, ".")
+print("The value of 3 + 4 is", 3+4 , ".", sep='')
+print("On the first line")
+print("On the second line")
+print("On the first line", end='')
+print("On the second line")
